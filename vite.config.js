058b30ec2a3e9d99e42d4d5d64129a6c1869d529
hillary nicholas <75504607+hill-nicholas@users.noreply.github.com>
2024-10-03 20:01:14 +0100

    make changes
    made some changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    base: mode === "production" ? "/job-portal/" : "/", // Adjust according to your repo name
+  };
 });
